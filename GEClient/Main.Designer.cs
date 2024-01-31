d834dd6d6ca43191d1e03293875768ff2caa7936
hhl <hoilung@foxmail.com>
2024-01-31 10:28:13 +0800

    添加项目文件。

---
+++ GEClient/Main.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace GEClient
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.tbx_port = new System.Windows.Forms.MaskedTextBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_ip = new System.Windows.Forms.TextBox();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.lv_data = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbx_address = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbx_addressOne = new System.Windows.Forms.TextBox();
+            this.cbx_type = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbx_value = new System.Windows.Forms.TextBox();
+            this.btn_readOne = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssl_tip = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbx_port);
+            this.groupBox1.Controls.Add(this.btn_close);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbx_ip);
+            this.groupBox1.Controls.Add(this.btn_open);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(695, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GE设置";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(411, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "最大字节:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(476, 21);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 21);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(529, 20);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 6;
+            this.btn_add.Text = "添加";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(610, 20);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(75, 23);
+            this.btn_read.TabIndex = 5;
+            this.btn_read.Text = "读取";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // tbx_port
+            // 
+            this.tbx_port.Location = new System.Drawing.Point(189, 22);
+            this.tbx_port.Mask = "99999";
+            this.tbx_port.Name = "tbx_port";
+            this.tbx_port.Size = new System.Drawing.Size(54, 21);
+            this.tbx_port.TabIndex = 2;
+            this.tbx_port.Text = "18245";
+            this.tbx_port.ValidatingType = typeof(int);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(330, 21);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 4;
+            this.btn_close.Text = "关闭";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // tbx_ip
+            // 
+            this.tbx_ip.Location = new System.Drawing.Point(42, 22);
+            this.tbx_ip.Name = "tbx_ip";
+            this.tbx_ip.Size = new System.Drawing.Size(100, 21);
+            this.tbx_ip.TabIndex = 1;
+            this.tbx_ip.Text = "192.168.1.100";
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(249, 21);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(75, 23);
+            this.btn_open.TabIndex = 3;
+            this.btn_open.Text = "连接";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // lv_data
+            // 
+            this.lv_data.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lv_data.FullRowSelect = true;
+            this.lv_data.GridLines = true;
+            this.lv_data.HideSelection = false;
+            this.lv_data.LabelEdit = true;
+            this.lv_data.Location = new System.Drawing.Point(6, 20);
+            this.lv_data.MultiSelect = false;
+            this.lv_data.Name = "lv_data";
+            this.lv_data.Size = new System.Drawing.Size(399, 285);
+            this.lv_data.TabIndex = 6;
+            this.lv_data.UseCompatibleStateImageBehavior = false;
+            this.lv_data.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Address";
+            this.columnHeader2.Width = 87;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Type";
+            this.columnHeader3.Width = 108;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Value";
+            this.columnHeader4.Width = 106;
+            // 
+            // tbx_address
+            // 
+            this.tbx_address.Location = new System.Drawing.Point(411, 49);
+            this.tbx_address.Multiline = true;
+            this.tbx_address.Name = "tbx_address";
+            this.tbx_address.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbx_address.Size = new System.Drawing.Size(272, 256);
+            this.tbx_address.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_readOne);
+            this.groupBox2.Controls.Add(this.tbx_value);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.cbx_type);
+            this.groupBox2.Controls.Add(this.tbx_addressOne);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(695, 57);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "单个查询";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.statusStrip1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lv_data);
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.tbx_address);
+            this.groupBox3.Controls.Add(this.btn_read);
+            this.groupBox3.Controls.Add(this.btn_add);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 117);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(695, 333);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "批量查询";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "地址:";
+            // 
+            // tbx_addressOne
+            // 
+            this.tbx_addressOne.Location = new System.Drawing.Point(54, 23);
+            this.tbx_addressOne.Name = "tbx_addressOne";
+            this.tbx_addressOne.Size = new System.Drawing.Size(100, 21);
+            this.tbx_addressOne.TabIndex = 1;
+            this.tbx_addressOne.Text = "M100";
+            // 
+            // cbx_type
+            // 
+            this.cbx_type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_type.FormattingEnabled = true;
+            this.cbx_type.Items.AddRange(new object[] {
+            "Bool",
+            "Int16",
+            "Int32",
+            "Float"});
+            this.cbx_type.Location = new System.Drawing.Point(203, 23);
+            this.cbx_type.Name = "cbx_type";
+            this.cbx_type.Size = new System.Drawing.Size(121, 20);
+            this.cbx_type.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(160, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "类型:";
+            // 
+            // tbx_value
+            // 
+            this.tbx_value.Location = new System.Drawing.Point(413, 23);
+            this.tbx_value.Name = "tbx_value";
+            this.tbx_value.Size = new System.Drawing.Size(109, 21);
+            this.tbx_value.TabIndex = 4;
+            // 
+            // btn_readOne
+            // 
+            this.btn_readOne.Location = new System.Drawing.Point(330, 22);
+            this.btn_readOne.Name = "btn_readOne";
+            this.btn_readOne.Size = new System.Drawing.Size(75, 23);
+            this.btn_readOne.TabIndex = 5;
+            this.btn_readOne.Text = "读取";
+            this.btn_readOne.UseVisualStyleBackColor = true;
+            this.btn_readOne.Click += new System.EventHandler(this.btn_readOne_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssl_tip});
+            this.statusStrip1.Location = new System.Drawing.Point(3, 308);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(689, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssl_tip
+            // 
+            this.tssl_tip.Name = "tssl_tip";
+            this.tssl_tip.Size = new System.Drawing.Size(32, 17);
+            this.tssl_tip.Text = "就绪";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GESRTP Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_ip;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.MaskedTextBox tbx_port;
+        private System.Windows.Forms.ListView lv_data;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox tbx_address;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbx_addressOne;
+        private System.Windows.Forms.ComboBox cbx_type;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbx_value;
+        private System.Windows.Forms.Button btn_readOne;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssl_tip;
+    }
+}
+
---
+++ GEClient/Main.cs
@@ -0,0 +1,131 @@
+﻿using HL.GESRTP;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GEClient
+{
+    public partial class Main : Form
+    {
+        private SRTP SRTP = null;
+        public Main()
+        {
+            InitializeComponent();
+            this.Text += " v" + this.ProductVersion;
+            changeState(false);
+            cbx_type.SelectedIndex = 0;
+        }
+        void changeState(bool state)
+        {
+            btn_open.Enabled = !state;
+            btn_readOne.Enabled = btn_add.Enabled = this.btn_close.Enabled = this.btn_read.Enabled = state;
+        }
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            SRTP = new SRTP(this.tbx_ip.Text);
+            try
+            {
+                var result = SRTP.Open() == 1;
+                changeState(result);
+                tssl_tip.Text = "连接成功";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            if (SRTP.Connected)
+            {
+                SRTP.Close();
+                SRTP = null;
+                changeState(false);
+                tssl_tip.Text = "连接已关闭";
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            var addrArry = tbx_address.Text.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var array = addrArry.Select(m => GEDataItem.ParseFrom(m, !m.StartsWith("A"))).ToList();
+            NewMethod(array);
+            lv_data.Tag = array;
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            var array = lv_data.Tag as List<GEDataItem>;
+            var st = Stopwatch.StartNew();
+            st.Start();
+            SRTP.ReadMultipleVars(array.ToArray(), (int)numericUpDown1.Value);
+            st.Stop();
+            tssl_tip.Text = $"用时：{st.ElapsedMilliseconds}ms";
+
+            NewMethod(array);
+            
+        }
+
+        private void NewMethod(List<GEDataItem> array)
+        {
+            lv_data.BeginUpdate();
+            lv_data.Items.Clear();
+            for (int i = 0; i < array.Count(); i++)
+            {
+                var item = new ListViewItem(i.ToString());
+                if (array[i].Value == null)
+                {
+                    if (array[i].IsBit)
+                    {
+                        array[i].Value = false;
+                    }
+                    else
+                    {
+                        array[i].Value = 0;
+                    }
+                }
+                item.SubItems.Add(array[i].Address);
+                item.SubItems.Add(array[i].IsBit ? "bool" : "int");
+                item.SubItems.Add(array[i].Value == null ? string.Empty : array[i].Value.ToString());
+                lv_data.Items.Add(item);
+            }
+            lv_data.EndUpdate();
+        }
+
+        private void btn_readOne_Click(object sender, EventArgs e)
+        {
+            this.tbx_value.Text = string.Empty;
+            var address = tbx_addressOne.Text.ToUpper();
+            object result = null;
+            var st = Stopwatch.StartNew();
+            st.Start();
+            switch (cbx_type.Text.ToLower())
+            {
+                case "bool":
+                    result = SRTP.ReadBoolean(address);
+                    break;
+                case "int16":
+                    result = SRTP.ReadInt16(address);
+                    break;
+                case "int32":
+                    result = SRTP.ReadInt32(address);
+                    break;
+                case "float":
+                    result = SRTP.ReadFloat(address);
+                    break;
+                default:
+                    MessageBox.Show("尚未支持的类型");
+                    break;
+            }
+            st.Stop();
+            tssl_tip.Text = $"用时：{st.ElapsedMilliseconds}ms";
+            tbx_value.Text = result.ToString();
+        }
+    }
+
+
+}
